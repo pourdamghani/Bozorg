6f448d3f352b5a44f4e3143dca93ae9b5cf61f04
pourdamghani <pourdamghani@gmail.com>
2015-05-12 11:42:07 +0430

    Power SetUp changed :) :D :)

--- src/Judge/Judge.java
+++ src/Judge/Judge.java
@@ -12,4 +12,17 @@ abstract public class Judge extends JudgeAbstract {
     public static int FAN_INCREASE_VALUE = 3;
     public static int MAX_Health = 100;
     public static int TIMEINTERVAL = 20;
+    public static int Reza_Vision = 6;
+    public static int Other_Player_Vision = 3;
+    public static int Jafar_Speed = 5;
+    public static int Other_Player_Speed = 2;
+    public static int Hasin_Power = 5;
+    public static int Saman_Power = 5;
+    public static int Jafar_Power = 1;
+    public static int Reza_Power = 4;
+    public static int Hasin_Fans = 10;
+    public static int Saman_Fans = 100;
+    public static int Jafar_Fans = 5;
+    public static int Reza_Fans = 0;
+
 }
