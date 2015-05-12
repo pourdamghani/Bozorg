b409d86416d71e418891ade245ee1d5ae63b6076
pourdamghani <pourdamghani@gmail.com>
2015-05-12 11:22:17 +0430

    Merge branch 'master' of C:\Users\Arash\Desktop\AP\Bozorg with conflicts.

--- src/Model/Player.java
+++ src/Model/Player.java
@@ -9,22 +9,21 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class Player {
-    private HashMap<String, Integer> information = new HashMap<String, Integer>();
+	private HashMap<String,Integer> information = new HashMap<String,Integer>();
     private ArrayList<Fan> aliveFans = new ArrayList<Fan>();
 
-    private Integer pendingAction, pendingPrize;
+	private Integer pendingAction, pendingPrize;
     private Integer prizeType;
 
     //check pendingAction
 
-    public void setInfo(String key, Integer value) {
-        information.put(key, value);
-    }
-
-    public void move(int direction) {
+	public void setInfo(String key, Integer value) {
+		information.put(key, value);
+	}
+	public void move(int direction){
         Integer row = information.get(JudgeAbstract.ROW);
         Integer col = information.get(JudgeAbstract.COL);
-        switch (direction) {
+        switch (direction){
             case JudgeAbstract.UP:
                 row--;
                 break;
@@ -38,57 +37,55 @@ public class Player {
                 col++;
                 break;
         }
-        information.put(JudgeAbstract.ROW, row);
-        information.put(JudgeAbstract.COL, col);
+        information.put(JudgeAbstract.ROW,row);
+        information.put(JudgeAbstract.COL,col);
 
-    }
-
-    public void getAttacked(Integer power) {
-        Integer newHealth = information.get(JudgeAbstract.HEALTH) - power;
-        information.put(JudgeAbstract.HEALTH, newHealth);
-        if (newHealth < 0) {
+	}
+	public void getAttacked(Integer power){
+		Integer newHealth = information.get(JudgeAbstract.HEALTH)-power;
+        information.put(JudgeAbstract.HEALTH,newHealth);
+        if(newHealth < 0){
             information.put(JudgeAbstract.IS_ALIVE, JudgeAbstract.DEAD);
-            information.put(JudgeAbstract.IS_WINNER, JudgeAbstract.LOST);
+            information.put(JudgeAbstract.IS_WINNER,JudgeAbstract.LOST);
         }
-    }
-
-    public Fan throwFan() {
-        Fan fan = new Fan(information.get(JudgeAbstract.ROW), information.get(JudgeAbstract.COL), information.get(JudgeAbstract.NAME), JudgeAbstract.ALIVE);
+	}
+	public Fan throwFan() {
+		Fan fan = new Fan(information.get(JudgeAbstract.ROW),information.get(JudgeAbstract.COL),information.get(JudgeAbstract.NAME),JudgeAbstract.ALIVE);
         aliveFans.add(fan);
         information.put(JudgeAbstract.FANS, information.get(JudgeAbstract.FANS));
         return fan;
-    }
+	}
 
     public void removeFan(Fan fan) {
         aliveFans.remove(fan);
     }
 
-    public void getGift(Integer giftType) {
+	public void getGift(Integer giftType){
         prizeType = giftType;
         switch (giftType) {
             case JudgeAbstract.SPEEDUP_CELL:
-                information.put(JudgeAbstract.SPEED, information.get(JudgeAbstract.SPEED) * 2);
-                pendingPrize = Judge.SPEEDUP_TIME * Judge.TIMEINTERVAL;
+                information.put(JudgeAbstract.SPEED,information.get(JudgeAbstract.SPEED)*2);
+                pendingPrize = Judge.SPEEDUP_TIME*Judge.TIMEINTERVAL;
                 break;
             case JudgeAbstract.RADAR_CELL:
-                pendingPrize = Judge.RADAR_TIME * Judge.TIMEINTERVAL;
+                pendingPrize = Judge.RADAR_TIME*Judge.TIMEINTERVAL;
                 break;
             case JudgeAbstract.STONE_CELL:
-                pendingPrize = Judge.STONE_TIME * Judge.TIMEINTERVAL;
+                pendingPrize = Judge.STONE_TIME*Judge.TIMEINTERVAL;
                 break;
             case JudgeAbstract.JUMP_CELL:
-                pendingPrize = Judge.JUMP_TIME * Judge.TIMEINTERVAL;
+                pendingPrize = Judge.JUMP_TIME*Judge.TIMEINTERVAL;
                 break;
             case JudgeAbstract.HOSPITAL_CELL:
-                information.put(JudgeAbstract.HEALTH, Math.min(Judge.MAX_Health, information.get(JudgeAbstract.HEALTH) + Judge.HEALTH_INCREASE_VALUE));
+                information.put(JudgeAbstract.HEALTH,Math.min(Judge.MAX_Health, information.get(JudgeAbstract.HEALTH)+Judge.HEALTH_INCREASE_VALUE ));
                 break;
             case JudgeAbstract.FAN_CELL:
-                information.put(JudgeAbstract.FANS, information.get(JudgeAbstract.FANS) + Judge.FAN_INCREASE_VALUE);
+
+                information.put(JudgeAbstract.FANS,information.get(JudgeAbstract.FANS)+Judge.FAN_INCREASE_VALUE);
                 break;
         }
     }
-
-    public ArrayList<String> getVision(Integer width, Integer height) {
+    public ArrayList<String> getVision(Integer width,Integer height){
         ArrayList<String> canSee = new ArrayList<String>();
         if (prizeType != JudgeAbstract.RADAR_CELL) {
             Integer vision = information.get(JudgeAbstract.VISION);
@@ -96,36 +93,36 @@ public class Player {
             Integer y = information.get(JudgeAbstract.COL);
             for (int i = x - vision; i <= x + vision; i++)
                 for (int j = y - vision; j <= y + vision; j++)
-                    if (i >= 0 && j >= 0 && i < height && j < width)
-                        canSee.add(i + "," + j);
-        } else {
-            for (int i = 0; i < height; i++)
-                for (int j = 0; j < width; j++)
+                    if( i >= 0 && j >= 0 && i<height && j<width)
                     canSee.add(i + "," + j);
+        }
+        else {
+            for(int i = 0; i < height; i++)
+                for(int j=0; j< width; j++)
+                    canSee.add(i + ","+ j);
         }
         return canSee;
     }
 
-    public ArrayList<Fan> getFans() {
-        return aliveFans;
-    }
-
-    public HashMap<String, Integer> getInfo() {
-        return information;
-    }
-
-    public void updateInfo(String infoKey, Integer infoValue) {
-        information.put(infoKey, infoValue);
-    }
-
-    public Integer getInfo(String infoKey) {
+	public ArrayList<Fan> getFans(){
+		return aliveFans;
+	}
+	public HashMap<String,Integer> getInfo(){
+		return information;
+	}
+	public void updateInfo(String infoKey,Integer infoValue){
+		information.put(infoKey,infoValue);
+	}
+    public Integer getInfo(String infoKey){
         return information.get(infoKey);
     }
 
-    public void reduce() {
+    public  void reduce(){
         //todo
 
     }
+
+    //todo pending action and pending prize 
 
     public Integer getPendingAction() {
         return pendingAction;
