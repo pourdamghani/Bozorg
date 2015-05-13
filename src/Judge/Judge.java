86fa94bcf82d30456fe2db41ade598f77af64d0f
malekan <immalekan@gmail.com>
2015-05-13 20:46:31 +0430

    Judge class completed I think please check it :D

--- src/Judge/Judge.java
+++ src/Judge/Judge.java
@@ -1,9 +1,24 @@
 package Judge;
 
+import Common.GameObjectID;
+import Common.exceptions.BozorgExceptionBase;
+import Controller.ControlUnit;
+import Model.Fan;
+import Model.Person;
+import Model.Player;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
 /**
  * Created by yashardabiran on 5/10/15.
  */
-abstract public class Judge extends JudgeAbstract {
+public class Judge extends JudgeAbstract {
+    private HashMap<GameObjectID, Person> IDtoPerson = new HashMap<>();
+    private HashMap<Person, GameObjectID> PersontoID = new HashMap<>();
+    private ControlUnit cu = new ControlUnit();
+
     public static int SPEEDUP_TIME = 5;
     public static int RADAR_TIME = 3;
     public static int STONE_TIME = 3;
@@ -24,5 +39,102 @@ abstract public class Judge extends JudgeAbstract {
     public static int Saman_Fans = 100;
     public static int Jafar_Fans = 5;
     public static int Reza_Fans = 0;
+    public ArrayList<GameObjectID> loadMap(int[][] cellsType, int[][] wallsType, int[] players) {
+        ArrayList<Player> player = cu.loadMap(cellsType, wallsType, players);
+        ArrayList<GameObjectID> ids = new ArrayList<>();
+        for (Player i : player) {
+            GameObjectID ID = GameObjectID.create(Player.class);
+            IDtoPerson.put(ID, i);
+            PersontoID.put(i, ID);
+            ids.add(ID);
+        }
+        return ids;
+    }
+    public int getMapWidth(){
+        return cu.getWidth();
+    }
+    public int getMapHeight(){
+        return cu.getHeight();
+    }
+    public int getMapCellType(int col, int row){
+        return cu.getMapCellType(row, col);
+    }
+    public int getMapCellType(int col, int row, GameObjectID player){
+        return cu.getMapCellType(row, col, (Player)IDtoPerson.get(player));
+    }
+    public int getMapWallType(int col, int row){
+        return cu.getMapWallType(row, col);
+    }
+    public int getMapWallType(int col, int row, GameObjectID player){
+        return cu.getMapWallType(row, col, (Player) IDtoPerson.get(player));
+    }
+    public void setup(){
+        cu.setup();
+    }
+    public void movePlayer(GameObjectID player, int direction) throws BozorgExceptionBase {
+        cu.movePlayer((Player) IDtoPerson.get(player), direction);
+    }
+    public void attack(GameObjectID attacker, int direction) throws BozorgExceptionBase{
+        cu.attack((Player) IDtoPerson.get(attacker), direction);
+    }
+    public GameObjectID throwFan(GameObjectID player) throws BozorgExceptionBase{
+        Fan fan = cu.throwfan((Player)IDtoPerson.get(player));
+        GameObjectID ID = GameObjectID.create(Fan.class);
+        IDtoPerson.put(ID, fan);
+        PersontoID.put(fan, ID);
+        return ID;
+    }
+    public void getGift(GameObjectID player) throws BozorgExceptionBase{
+        cu.getGift((Player) IDtoPerson.get(player));
+    }
+    public void AIByStudents(GameObjectID player){
+        //TODO
+    }
+    public void AIByTAs(GameObjectID player){
+        //TODO by TA
+    }
+    public ArrayList<GameObjectID> getEveryThing(){
+        //TODO
+        //NOT USE IN JUDGE
+        return null;
+    }
+    public ArrayList<String> getVision(GameObjectID player) throws BozorgExceptionBase{
+        return cu.getVision((Player) IDtoPerson.get(player));
+    }
+    public ArrayList<GameObjectID> getPlayersInVision ( GameObjectID player){
+        ArrayList<Player> players = cu.getPlayersInVision((Player) IDtoPerson.get(player));
+        ArrayList<GameObjectID> IDs = new ArrayList<>();
+        for(Player i: players)
+            IDs.add(PersontoID.get(i));
+        return IDs;
+    }
+    public ArrayList<GameObjectID> getFans(GameObjectID player) throws BozorgExceptionBase{
+        ArrayList<Fan> fans = cu.getFans((Player) IDtoPerson.get(player));
+        ArrayList<GameObjectID> IDs = new ArrayList<>();
+        for(Fan i: fans)
+            IDs.add(PersontoID.get(i));
+        return IDs;
+    }
+    public HashMap<String, Integer> getInfo(GameObjectID id) throws BozorgExceptionBase{
+        return cu.getInfo(IDtoPerson.get(id));
+    }
+    public void next50milis(){
+        cu.next50milies();
+    }
+    public void startTimer(){
+        //TODO
+        //NEVER USE IN JUDGE
+    }
+    public  void pauseTimer(){
+        //TODO
+        //NEVER USE IN JUDGE
+    }
+    //check it please
+    public float getTime(){
+        return cu.getTime();
+    }
+    public void updateInfo(GameObjectID id, String infoKey,Integer infoValue) throws BozorgExceptionBase{
+        //TODO
+    }
 
 }
