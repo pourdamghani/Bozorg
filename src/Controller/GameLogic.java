d0570a22e722c4c48d431e58376d5f4e67973871
Yashar <yashardabiran75@gmail.com>
2015-05-19 17:29:52 +0430

    debugged canMove method and a little bit of refactoring

--- src/Controller/GameLogic.java
+++ src/Controller/GameLogic.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import Judge.Judge;
 import Judge.JudgeAbstract;
 import Model.Cell;
 import Model.Fan;
@@ -9,7 +10,14 @@ import Model.Player;
 import java.util.ArrayList;
 
 public class GameLogic {
-    //if player cant move or attack in spacial direction beacuse of wall return false else return true
+
+    /**
+     * Checking if there is a wall in specific direction
+     * @param player
+     * @param direction
+     * @param map the map player is playing on
+     * @return true if there isn't a wall and false otherwise
+     */
     private boolean isNoWall(Player player, int direction, Map map) {
         Cell cell = map.getCell(player.getInfo(JudgeAbstract.ROW), player.getInfo(JudgeAbstract.COL));
         switch (direction) {
@@ -34,11 +42,20 @@ public class GameLogic {
         return true;
     }
 
-    //number of player in (x,y) cell
-    private int numberofPlayer(int X, int Y, ArrayList<Player> players, int width, int height) {
+    /**
+     *
+     * @param X
+     * @param Y
+     * @param players
+     * @param width of the map
+     * @param height height of the map
+     * @return number of the players existing in cell (X, Y)
+     */
+    private int numberOfPlayers(int X, int Y, ArrayList<Player> players, int width, int height) {
         int counter = 0;
-        if (X >= width || Y >= height || X < 0 || Y < 0)
+        if (X >= width || Y >= height || X < 0 || Y < 0) //Checking if X and Y are valid
             return counter;
+
         for (Player player : players)
             if (player.getInfo(JudgeAbstract.ROW) == X && player.getInfo(JudgeAbstract.COL) == Y)
                 counter++;
@@ -46,29 +63,29 @@ public class GameLogic {
     }
 
     //if there are on player to attack return true else return false
-    private boolean playerexist(int direction,Player player, ArrayList<Player> players, Map map) {
+    private boolean playerExists(int direction, Player player, ArrayList<Player> players, Map map) {
         int X = player.getInfo(JudgeAbstract.ROW), Y = player.getInfo(JudgeAbstract.COL);
         int width = map.getWidth();
         int height = map.getHeight();
         switch (direction) {
             case JudgeAbstract.UP:
-                if (numberofPlayer(X, Y + 1, players, width, height) == 0)
+                if (numberOfPlayers(X, Y + 1, players, width, height) == 0)
                     return false;
                 break;
             case JudgeAbstract.DOWN:
-                if (numberofPlayer(X, Y - 1, players, width, height) == 0)
+                if (numberOfPlayers(X, Y - 1, players, width, height) == 0)
                     return false;
                 break;
             case JudgeAbstract.LEFT:
-                if (numberofPlayer(X - 1, Y, players, width, height) == 0)
+                if (numberOfPlayers(X - 1, Y, players, width, height) == 0)
                     return false;
                 break;
             case JudgeAbstract.RIGHT:
-                if (numberofPlayer(X + 1, Y, players, width, height) == 0)
+                if (numberOfPlayers(X + 1, Y, players, width, height) == 0)
                     return false;
                 break;
             case JudgeAbstract.NONE:
-                if (numberofPlayer(X, Y, players, width, height) <= 1)
+                if (numberOfPlayers(X, Y, players, width, height) < 2)
                     return false;
                 break;
         }
@@ -76,14 +93,36 @@ public class GameLogic {
     }
 
     public boolean canMove(Player player, int direction, Map map) {
+        if (player.getInfo(JudgeAbstract.IS_ALIVE) != JudgeAbstract.ALIVE)
+            return false;
+        if (player.getPendingAction() != 0) //Checks if any move is in queue
+            return false;
+
+        int x = player.getInfo(JudgeAbstract.ROW);
+        int y = player.getInfo(JudgeAbstract.COL);
+        switch (direction) { //Checks if move is not going out of the board
+            case JudgeAbstract.UP:
+                if (x - 1 < 0)
+                    return false;
+                break;
+            case JudgeAbstract.RIGHT:
+                if (y + 1 >= map.getHeight())
+                    return false;
+                break;
+            case JudgeAbstract.DOWN:
+                if (x + 1 >= map.getWidth())
+                    return false;
+                break;
+            case JudgeAbstract.LEFT:
+                if (y - 1 < 0)
+                    return false;
+                break;
+        }
+
         if (player.getPrizeType() == JudgeAbstract.STONE_CELL)
             return false;
         if (player.getPrizeType() == JudgeAbstract.JUMP_CELL)
             return true;
-        if (player.getInfo(JudgeAbstract.IS_ALIVE) != JudgeAbstract.ALIVE)
-            return false;
-        if (player.getPendingAction() != 0)//havnt got any action
-            return false;
         if (!isNoWall(player, direction, map))
             return false;
         return true;
@@ -99,7 +138,7 @@ public class GameLogic {
             return false;
         if (!isNoWall(player, direction, map))
             return false;
-        if (!playerexist(direction, player, players, map))
+        if (!playerExists(direction, player, players, map))
             return false;
         return true;
     }
