9b00c2a3d8f98aa0d5ac8b5e912c79d0f57398ad
malekan <immalekan@gmail.com>
2015-07-11 17:00:10 +0430

    Merge branch 'master' of C:\Users\PARDAZESH\IdeaProjects\Bozorg with conflicts.

--- src/gamePanel/BozorgPanel.java
+++ src/gamePanel/BozorgPanel.java
@@ -1,19 +1,159 @@
-package gamePanel;
+package graphic;
 
+import Controller.GameEngine;
 import Judge.JudgeAbstract;
-import gameController.GameController;
-import gameEngine.GameEngine;
-import gameEngine.Model.Fan;
-import gameEngine.Model.Player;
+import Model.Fan;
+import Model.Player;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
-/**
- * Created by yashardabiran on 7/11/15.
- */
-class BozorgPanel extends JPanel {
+public class GamePanel extends JPanel {
+    /**
+     *
+     */
+    private BozorgPanel bozorgPanel;
+    private InformationPanel informationPanel;
+
+    private JComboBox comboBox;
+
+
+    public void init(final GameController controller, GameEngine engine) {
+        setLayout(new BorderLayout());
+        setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        //set the name for comboBox
+        ArrayList<Player> players = engine.getPlayers();
+        String s[] = new String[players.size() + 2];
+        s[0] = "ALL";
+        for(int i = 1; i <= players.size(); i++) {
+            int name = players.get(i - 1).getInfo(JudgeAbstract.NAME);
+            switch (name) {
+                case JudgeAbstract.SAMAN:
+                    s[i] = "SAMAN";
+                    break;
+                case JudgeAbstract.JAFAR:
+                    s[i] = "JAFAR";
+                    break;
+                case JudgeAbstract.HASIN:
+                    s[i] = "HASIN";
+                    break;
+                case JudgeAbstract.REZA:
+                    s[i] = "REZA";
+                    break;
+            }
+        }
+        s[players.size() + 1] = "BOTH";
+        comboBox = new JComboBox(s);
+
+        add(comboBox, BorderLayout.PAGE_START);
+
+        comboBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                bozorgPanel.setPlayer((String) comboBox.getSelectedItem());
+            }
+        });
+
+
+        bozorgPanel = new BozorgPanel();
+        bozorgPanel.init(controller, engine);
+        add(bozorgPanel, BorderLayout.CENTER);
+
+        informationPanel = new InformationPanel(engine);
+        add(informationPanel, BorderLayout.LINE_START);
+    }
+
+    public int showGameOverMessage() {
+        //set audio for end of the game
+        try {
+            InputStream in = new FileInputStream("src/Sound/victory.wav");
+            AudioStream audioStream = new AudioStream(in);
+            AudioPlayer.player.start(audioStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int n = JOptionPane.showConfirmDialog(this, "Victory! \n Would you like to start again?");
+
+        if (n > 0)
+            System.exit(0);
+
+        return n;
+    }
+
+    public void showCant(String s) {
+        JOptionPane.showMessageDialog(this, "You cant " + s + "!", "Watch Out", JOptionPane.ERROR_MESSAGE);
+    }
+
+    public String whichPlayer(){
+        if (comboBox.getSelectedItem().equals("ALL"))
+            return null;
+        return (String)comboBox.getSelectedItem();
+    }
+
+    public void paintAttack() {
+        try {
+            InputStream in = new FileInputStream("src/Sound/fight.wav");
+            AudioStream audioStream = new AudioStream(in);
+            AudioPlayer.player.start(audioStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void paintMove() {
+        try {
+            InputStream in = new FileInputStream("src/Sound/walk.wav");
+            AudioStream audioStream = new AudioStream(in);
+            AudioPlayer.player.start(audioStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showGift(int gift) {
+        String Message = "Nothing here";
+        switch (gift){
+            case JudgeAbstract.SPEEDUP_CELL:
+                Message = "You have Nitro for 5 sec,Speed Up!";
+                break;
+            case JudgeAbstract.HOSPITAL_CELL:
+                Message = "First Aid Kit!,health increases";
+                break;
+            case JudgeAbstract.FAN_CELL:
+                Message = "Every body loves You!, 3 extra fans";
+                break;
+            case JudgeAbstract.JUMP_CELL:
+                Message = "Magical shoes!, You Can Jump over the walls for 2 sec";
+                break;
+            case JudgeAbstract.STONE_CELL:
+                Message = "Your Stoned, Can't play for 3 sec";
+                break;
+            case JudgeAbstract.RADAR_CELL:
+                Message = "Sun rises, You can See EveryWhere for 3 sec";
+                break;
+        }
+        JOptionPane.showMessageDialog(this, Message, "Gift", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
+
+class BozorgPanel extends JPanel{
 
 
     /**
@@ -58,13 +198,11 @@ class BozorgPanel extends JPanel {
         g2d.setColor(Color.BLUE);
         g2d.fillRect(0, 0, WIDTH, HEIGHT);
 
-//        setFocusable(true);
-//        requestFocus();
-
         paintMap(g2d);
+
         paintPlayers(g2d);
+
         paintFans(g2d);
-//        paintGifts(g2d);
     }
 
     private void paintMap(Graphics2D g2d){
@@ -101,10 +239,12 @@ class BozorgPanel extends JPanel {
                 }
                 paintCell(g2d, j * CellSize, i * CellSize, color);
             }
+
         for(int i = 0; i < engine.getHeight(); i++)
             for(int j = 0; j < engine.getWidth(); j++)
                 paintWall(g2d, i, j);
     }
+
     private void paintWall(Graphics2D g2d, int i, int j){
         int wall = engine.getMapWallType(i, j);
         g2d.setColor(Color.BLACK);
@@ -113,10 +253,12 @@ class BozorgPanel extends JPanel {
         if (wall % 8 > 3)
             g2d.fillRect((j) * CellSize, (i + 1) * (CellSize), CellSize, 3);
     }
+
     private void paintCell(Graphics2D g2d,int x, int y, Color color){
         g2d.setColor(color);
         g2d.fillRect(x,y,CellSize,CellSize);
     }
+
     private void paintPlayers(Graphics2D g2d) {
         if (allMapSeen)
             for (Player i : engine.getPlayers())
@@ -134,14 +276,9 @@ class BozorgPanel extends JPanel {
 
     private void paintPlayer(Graphics2D g2d, Player player){
         g2d.drawImage(player.getImage(), (player.getInfo(JudgeAbstract.COL)) * CellSize, (player.getInfo(JudgeAbstract.ROW)) * CellSize, this);
-        //g2d.setColor(player.getColor());
-        //g2d.fillOval((player.getInfo(JudgeAbstract.COL)) * CellSize, (player.getInfo(JudgeAbstract.ROW)) * CellSize , CellSize, CellSize);
     }
 
     private void paintFans(Graphics2D g2d){
-//        if(!allMapSeen)
-//            for(Fan fan: engine.getFans(engine.stringToPlayer(player)))
-//                paintFan(g2d, fan);
         for(Fan fan: engine.getFans())
             if(allMapSeen)
                 paintFan(g2d, fan);
@@ -160,17 +297,7 @@ class BozorgPanel extends JPanel {
     private void paintFan(Graphics2D g2d, Fan fan){
         g2d.drawImage(fan.getImage(), (fan.getInfo(JudgeAbstract.COL)) * CellSize, (fan.getInfo(JudgeAbstract.ROW)) * CellSize, this);
         g2d.setColor(fan.getColor());
-        //g2d.fillRect(fan.getInfo(JudgeAbstract.COL) * CellSize + CellSize / 4 , fan.getInfo(JudgeAbstract.ROW) *CellSize + CellSize / 4, CellSize / 2, CellSize / 2);
     }
-
-//    private void paintGifts(Graphics2D g2d){
-//        for(int i = 0; i < engine.getHeight(); i++)
-//            for(int j = 0; j < engine.getWidth(); j++)
-//                if(allMapSeen || engine.getMapCellType(i, j, engine.stringToPlayer(player)) == JudgeAbstract.BONUS_CELL){
-//                    g2d.setColor(Color.red);
-//                    g2d.fillRect(j - CellSize / 2, i - CellSize / 2, CellSize, CellSize);
-//                }
-//    }
 
     public void setPlayer(String s){
         if(s.equals("ALL"))
@@ -187,6 +314,5 @@ class BozorgPanel extends JPanel {
         }
         setFocusable(true);
         requestFocus();
-
     }
 }
